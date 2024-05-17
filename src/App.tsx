e44bbd4a9d2f333aa28abd3a1405beaf77c90892
Arjun Dubey <arjundubeycr@gmail.com>
2024-05-18 00:04:15 +0530

    Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {data} from "./data/data.json";
+
+function App() {
+  const { AuthorWorklog } = data;
+  // Use the 'AuthorWorklog' value in your code here
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ src/types/AuthorWorkLog.ts
@@ -0,0 +1,48 @@
+export interface ActivityMeta {
+	label: string;
+	fillColor: string;
+}
+
+export interface TotalActivity {
+	name: string;
+	value: string;
+}
+
+export interface DayWiseActivityItem {
+	count: string;
+	label: string;
+	fillColor: string;
+}
+
+export interface DayWiseActivity {
+	date: string;
+	items: {
+		children: DayWiseActivityItem[];
+	};
+}
+
+export interface ActiveDays {
+	days: number;
+	isBurnOut: boolean;
+	insight: string[];
+}
+
+export interface Row {
+	name: string;
+	totalActivity: TotalActivity[];
+	dayWiseActivity: DayWiseActivity[];
+	activeDays: ActiveDays;
+}
+
+export interface AuthorWorklog {
+	activityMeta: ActivityMeta[];
+	rows: Row[];
+}
+
+export interface Data {
+	AuthorWorklog: AuthorWorklog;
+}
+
+export interface RootObject {
+	data: Data;
+}
